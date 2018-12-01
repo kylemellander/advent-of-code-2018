4e58f7749a49d68779dc3a68c202621003e8f3c0
Kyle Mellander <kylemellander@gmail.com>
2018-11-30 23:10:44 -0800

    clean up testing to be inline

--- lib/day1.ex
+++ lib/day1.ex
@@ -10,33 +10,52 @@ defmodule Day1 do
   For example, if the device displays frequency changes of +1, -2, +3, +1, then
   starting from a frequency of zero, the following changes would occur:
 
+  ```
   Current frequency  0, change of +1; resulting frequency  1.
   Current frequency  1, change of -2; resulting frequency -1.
   Current frequency -1, change of +3; resulting frequency  2.
   Current frequency  2, change of +1; resulting frequency  3.
   In this example, the resulting frequency is 3.
+  ```
 
   Here are other example situations:
 
+  ```
   +1, +1, +1 results in  3
   +1, +1, -2 results in  0
   -1, -2, -3 results in -6
+  ```
   """
 
   @doc """
-  star1/0
-
   For the first star of Day 1
 
   Starting with a frequency of zero, what is the resulting frequency after all
   of the changes in frequency have been applied?
+
+  ## Examples
+
+      iex> Day1.star1
+      574
   """
   def star1, do: star1(data())
   def star1(data) when is_list(data), do: Enum.reduce(data, 0, &sum/2)
+
+  @doc ~S"""
+  ## Examples
+
+      iex> Day1.star1("+1\n-2\n+3\n+1\n")
+      3
+      iex> Day1.star1("+1\n+1\n+1\n")
+      3
+      iex> Day1.star1("+1\n+1\n-2\n")
+      0
+      iex> Day1.star1("-1\n-2\n-3\n")
+      -6
+  """
   def star1(data), do: data |> parse_data |> star1
 
   @doc """
-  star2/0
   For the second star of Day 1
 
   You notice that the device repeats the same frequency change list over and
@@ -46,6 +65,7 @@ defmodule Day1 do
   For example, using the same list of changes above, the device would loop as
   follows:
 
+  ```
   Current frequency  0, change of +1; resulting frequency  1.
   Current frequency  1, change of -2; resulting frequency -1.
   Current frequency -1, change of +3; resulting frequency  2.
@@ -54,6 +74,7 @@ defmodule Day1 do
   Current frequency  3, change of +1; resulting frequency  4.
   Current frequency  4, change of -2; resulting frequency  2, which has already
   been seen.
+  ```
 
   In this example, the first frequency reached twice is 2. Note that your device
   might need to repeat its list of frequency changes many times before a
@@ -62,17 +83,21 @@ defmodule Day1 do
 
   Here are other examples:
 
+  ```
   +1, -1 first reaches 0 twice.
   +3, +3, +4, -2, -4 first reaches 10 twice.
   -6, +3, +8, +5, -6 first reaches 5 twice.
   +7, +7, -2, -7, -4 first reaches 14 twice.
+  ```
 
   What is the first frequency your device reaches twice?
   """
   def star2, do: star2(data())
+
   def star2(data) when is_list(data) do
     search_for_duplicate({:not_ok, 0, [0]}, data)
   end
+
   def star2(data), do: data |> parse_data |> star2
 
   defp sum(n, acc) do
--- test/day1_test.exs
+++ test/day1_test.exs
@@ -2,13 +2,6 @@ defmodule Day1Test do
   use ExUnit.Case
   doctest Day1
 
-  test "star1/0 finds the sum for test cases when input is raw" do
-    assert Day1.star1("+1\n-2\n+3\n+1\n") == 3
-    assert Day1.star1("+1\n+1\n+1\n") == 3
-    assert Day1.star1("+1\n+1\n-2\n") == 0
-    assert Day1.star1("-1\n-2\n-3\n") == -6
-  end
-
   test "star1/0 find the sum for test cases when input is parsed" do
     assert Day1.star1([1, 1, 1]) == 3
     assert Day1.star1([1, 1, -2]) == 0
